e520a94dce5d4147d7eb486c6ba027879aa99ace
Tomasz <tomasz.kubat@wp.pl>
2024-10-17 14:24:09 +0200

    updated README

---
+++ src/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+docs_url = "/api/docs"
+redoc_url = "/api/redoc"
+
+app = FastAPI(
+    docs_url=docs_url,
+    redoc_url=redoc_url,
+    title="Home API",
+    description="My home API",
+    version="1.0.0"
+)
+
+
+@app.get("/", description="Root endpoint")
+def root():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": f"For docs see '{docs_url}', for redocs see '{redoc_url}'"})
